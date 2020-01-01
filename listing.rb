6105ec0f0cc4841b93032df6e2d30a4302cc47c1
soapia <sofia.ongele@ealas.org>
2020-01-01 14:37:00 -0800

    haven't looked at this gal in a hot second but apparently i changed smth??

--- listing.rb
+++ listing.rb
@@ -1,1 +1,23 @@
 require_relative 'host.rb'
+
+class Listing
+    attr_reader :host, :beds, :dates_available, :cancellation, :guests, :reviews, :price
+    attr_writer :host, :beds, :dates_available, :cancellation, :guests, :reviews, :price
+    def initialize(host, beds, dates_available, price)
+        @host = "host"
+        @beds = "beds"
+        @dates_available = "dates_available"
+        @price = "price"
+        @cancellation = "strict"
+        @guests = []
+        @reviews = {}
+    end
+    def reserve_and_pay(guest_name, nights)
+        @guests.push(guest_name)
+        payment = nights * price
+        return payment
+    end
+    def leave_review(guest_name, text)
+        @reviews.store(guest_name, text)
+    end
+end
